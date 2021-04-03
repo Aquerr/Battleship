4e42083af4659896a0b7df6d1409a4d884922da4
Aquerr <nerdianin@gmail.com>
2021-04-03 18:49:32 +0200

    Prepare project for development

---
+++ src/main/java/pl/bartlomiejstepien/battleship/BattleshipGame.java
@@ -0,0 +1,19 @@
+package pl.bartlomiejstepien.battleship;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class BattleshipGame extends Application
+{
+    public static void main(String[] args)
+    {
+        BattleshipGame.launch();
+    }
+
+    public void start(Stage primaryStage) throws Exception
+    {
+        // TODO: Tutaj napisz kod aplikacji!
+
+
+    }
+}
